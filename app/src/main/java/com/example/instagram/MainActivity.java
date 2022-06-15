2856a631aaf0fc695d823adddb34fbb94fbd32dd
David Oyekeye <doyekeye@fb.com>
2022-06-15 00:23:50 -0700

    Upload description for new post functionality done, upload image in next commit

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -8,15 +8,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -44,7 +48,19 @@ public class MainActivity extends AppCompatActivity {
         submitBtn = findViewById(R.id.submitBtn);
         uploadBtn = findViewById(R.id.uploadBtn);
         
-        queryPost();
+//        queryPost();
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String description = etDescription.getText().toString();
+                if (description.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Put description", Toast.LENGTH_SHORT).show();
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                savePost(description,currentUser);
+
+            }
+        });
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener()  {
             @Override
@@ -66,6 +82,23 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    public void savePost(String description, ParseUser currentUser) {
+            Post post = new Post();
+            post.setDescription(description);
+            post.setUser(currentUser);
+            post.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e!=null){
+                        Log.e("Savingdescription","error saving",e);
+                        Toast.makeText(MainActivity.this, "Error saving", Toast.LENGTH_SHORT).show();
+                    }
+                    Log.i("Save","Post description successful");
+                    etDescription.setText("");
+                }
+            });
     }
 
     private void queryPost() {
